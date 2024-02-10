6aa8b663f2ecf6ba8c89724e94ba3a73b4431800
erika-tay <147895704+erika-tay@users.noreply.github.com>
2024-02-10 16:38:13 -0500

    CategoryList.css

--- hacklytics/src/pages/CategoryList.js
+++ hacklytics/src/pages/CategoryList.js
@@ -1,15 +1,21 @@
 import Movie from '../components/Movie';
 import Random from '../components/Random';
 import Music from '../components/Music';
-
+import "../pages/CategoryList.css"
 import React from 'react';
 
 export default function CategoryList() {
     return (
-        <div>
-            <Movie />
-            <Random />
-            <Music />
+        <div className = 'parent'> 
+            <div className = 'child1'> 
+                <Movie />
+            </div>
+            <div className ='child2'>
+                <Random />
+            </div>
+            <div className='child3'>
+                <Music /> 
+            </div>
         </div>
       );
 }

54802a38e2d545982dcaaabbd74cf4dd4c03ac51
Alex Li <ali602@gatech.edu>
2024-02-10 19:30:34 -0500

    pushed dropdown changes

--- hacklytics/src/pages/BingeList.js
+++ hacklytics/src/pages/BingeList.js
@@ -3,13 +3,41 @@ import DropList from './DropList.js';
 import Generate from './Generate.js';
 import React from 'react';
 import './BingeList.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function BingeList() {
     const [selectedOption, setSelectedOption] = useState(null);
     const [genre, setGenre] = useState('Genre');
     const [mood, setMood] = useState('Mood');
     const [recommendation, setRecommendation] = useState('Your recommendation here');
+    const [genresList, setGenresList] = useState([]);
+
+    useEffect(() => {
+        // Logic to update genresList when selectedOption changes
+        if (selectedOption === 'movies') {
+            const genres = [
+                "Action", "Adult", "Adventure", "Animation", "Biography", "Comedy", "Crime",
+                "Documentary", "Drama", "Family", "Fantasy", "Film-Noir", "Game-Show",
+                "History", "Horror", "Music", "Musical", "Mystery", "News", "Reality-TV",
+                "Romance", "Sci-Fi", "Short", "Sport", "Talk-Show", "Thriller", "War", "Western"
+            ];
+            setGenresList(genres);
+            setGenre(genres[0]); // Set the initial genre to the first one in the list
+        } else if (selectedOption === 'music') {
+            const genres = [
+                "Classical", "Rock", "Stuff"
+            ];
+            setGenresList(genres);
+            setGenre(genres[0]); // Set the initial genre to the first one in the list
+        } else if (selectedOption === 'random') {
+            const genres = [
+                "RickRoll"
+            ];
+            setGenresList(genres);
+            setGenre(genres[0]); // Set the initial genre to the first one in the list
+        }
+        console.log(genresList);
+    }, [selectedOption]);
 
     const handleOptionSelect = (option) => {
         setSelectedOption(option);
@@ -22,14 +50,18 @@ export default function BingeList() {
         // Logic to generate recommendation based on selectedOption, genre, and mood
         // For now, let's just update the recommendation with a dummy text
         setRecommendation('Dummy recommendation');
-      };
+    };
 
     return (
         <div>
             <h1 className="header">BingeList</h1>
-            <CategoryList /> 
+            <CategoryList handleOptionSelect={handleOptionSelect}/> 
+            <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+                {genresList.map((item) => (
+                    <option value={item}>{item}</option>
+                ))}
+            </select>
             {/* <DropList /> */}
-            {/* <Generate />    */}
             <textarea className="recommendation-textbox" value={recommendation} readOnly />
             <button className="generate-button" onClick={handleGenerateRecommendation}>Generate</button>
         </div>
--- hacklytics/src/pages/CategoryList.js
+++ hacklytics/src/pages/CategoryList.js
@@ -1,24 +1,23 @@
 import Movie from '../components/Movie';
 import Random from '../components/Random';
 import Music from '../components/Music';
+import { ReactComponent as MoviePic } from '../images/movie.svg'; // Importing SVG image
+import { ReactComponent as MusicPic } from '../images/music.svg'; // Importing SVG image
+import { ReactComponent as RandomPic } from '../images/random.svg'; // Importing SVG image
+
 import "./CategoryList.css"
 import React from 'react';
 
-export default function CategoryList() {
+export default function CategoryList({ handleOptionSelect }) {
+    const handleClick = (option) => {
+        handleOptionSelect(option);
+    };
+
     return (
-        <div className="category-container">
-            <Movie />
-            <Music /> 
-            <Random />
+        <div className="container">
+            <button className="button" onClick={() => handleClick('movies')}><MoviePic className="image" />Movies</button>
+            <button className="button" onClick={() => handleClick('music')}><MusicPic className="image" />Music</button>
+            <button className="button" onClick={() => handleClick('random')}><RandomPic className="image" />Random</button>
         </div>
-        // <div className = 'parent'> 
-        //         <Movie />
-        //     <div className ='child2'>
-        //         <Random />
-        //     </div>
-        //     <div className='child3'>
-        //         <Music /> 
-        //     </div>
-        // </div>
       );
 }

ec9b938630849b9137b69dd21c34965a2d5603cf
Alex Li <ali602@gatech.edu>
2024-02-10 19:39:56 -0500

    added categorylist to add conditional rendering

--- hacklytics/src/pages/CategoryList.js
+++ hacklytics/src/pages/CategoryList.js
@@ -7,17 +7,26 @@ import { ReactComponent as RandomPic } from '../images/random.svg'; // Importing
 
 import "./CategoryList.css"
 import React from 'react';
+import { useState } from 'react';
 
 export default function CategoryList({ handleOptionSelect }) {
+    const [selectedOption, setSelectedOption] = useState(null);
+
     const handleClick = (option) => {
         handleOptionSelect(option);
+        setSelectedOption(option);
     };
-
     return (
         <div className="container">
-            <button className="button" onClick={() => handleClick('movies')}><MoviePic className="image" />Movies</button>
-            <button className="button" onClick={() => handleClick('music')}><MusicPic className="image" />Music</button>
-            <button className="button" onClick={() => handleClick('random')}><RandomPic className="image" />Random</button>
+            <button className={`button ${selectedOption === 'movies' ? 'selected' : ''}`} onClick={() => handleClick('movies')}>
+                <MoviePic className="image" />Movies
+            </button>
+            <button className={`button ${selectedOption === 'music' ? 'selected' : ''}`} onClick={() => handleClick('music')}>
+                <MusicPic className="image" />Music
+            </button>
+            <button className={`button ${selectedOption === 'random' ? 'selected' : ''}`} onClick={() => handleClick('random')}>
+                <RandomPic className="image" />Random
+            </button>
         </div>
       );
 }
